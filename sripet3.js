85d8eb76aa8c06e647a15be111f36fd0a4266024
chandra perusomula <chandra.perusomula@gmail.com>
2025-02-27 16:35:51 +0530

    Add files via upload

---
+++ sripet3.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu = document.querySelector('.manu');
+    const navmobile = document.querySelector('.nav-mobile');
+    menu.classList.toggle('active');
+    navmobile.classList.toggle('active');
+}
